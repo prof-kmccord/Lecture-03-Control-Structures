62bb7abfe9b59bc9cc9d6b7c4d4b57463d3a9b98
Kenneth McCord <kmccord@vt.edu>
2023-08-23 04:50:45 -0400

    updated for student exercise

--- Form1.vb
+++ Form1.vb
@@ -1,43 +1,25 @@
 ﻿Public Class Form1
     Private Sub CheckEligibilityButton_Click(sender As Object, e As EventArgs) Handles CheckEligibilityButton.Click
 
-        ' Read GPA from the textbox
-
-        Dim gpa As Double = CDbl(GpaTextBox.Text)
+        ' Declare and Read varialbe called 'gpa' from the textbox
 
 
 
-        ' Determine scholarship eligibility using a For loop
-
-        Dim eligible As Boolean = False
 
 
-
-        For year As Integer = 1 To 4
-
-            If gpa >= 3.5 Then
-
-                eligible = True
-
-                Exit For
-
-            End If
-
-        Next
+        ' Declare and set 'False' value for variable called 'eligible' to determine scholarship eligibility using a For loop
 
 
 
-        ' Display eligibility message
+        ' Implement For loop that looks at all four years of school. if the GPA is equal or above a 3.5 set the students eligible to True
 
-        If eligible Then
 
-            EligibilityStatusLabel.Text = "Congratulations! You are eligible for a scholarship."
 
-        Else
 
-            EligibilityStatusLabel.Text = "Sorry, you are not eligible for a scholarship."
 
-        End If
+        ' Display eligibility message for when the students GPA is eligible for a scholarship and when it is not
+
+
 
     End Sub
 End Class
